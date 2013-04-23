2f2157e4ac6c0e9c9c48f8b94660155e95048605
James Nocentini <james.nocentini@gmail.com>
2013-04-23 18:00:56 +0100

    added slideshow template

---
+++ PhoneGap/iOS/angular_slideshow/www/app/scripts/controllers/main.js
@@ -0,0 +1,27 @@
+'use strict';
+
+angular.module('appApp')
+  .controller('MainCtrl', function ($scope) {
+
+            $scope.awesomeThings = [
+              'HTML5 Boilerplate',
+              'AngularJS',
+              'Karma'
+            ];
+              
+              
+          $scope.images = [
+                                'http://lorempixel.com/400/200/city/0/',
+                                'http://lorempixel.com/400/200/city/1/',
+                                'http://lorempixel.com/400/200/city/2/',
+                                'http://lorempixel.com/400/200/city/3/',
+                                'http://lorempixel.com/400/200/city/4/',
+                                'http://lorempixel.com/400/200/city/5/',
+                                'http://lorempixel.com/400/200/city/6/'
+
+
+
+          ]
+              
+              
+  });
